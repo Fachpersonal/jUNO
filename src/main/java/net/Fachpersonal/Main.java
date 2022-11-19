4a0e8b4a77e3a2b3147a097d902270f9ef7e9420
Fachpersonal <falscherIdiot@gmail.com>
2022-11-19 14:37:38 +0100

    using DefaultUnoServer instead of creating one

--- src/main/java/net/Fachpersonal/Main.java
+++ src/main/java/net/Fachpersonal/Main.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, UNOException {
         System.out.println("Hello world!");
-        new UNOServer(12345,-1);
+        UNOServer.DefaultUNOServer();
     }
 }
