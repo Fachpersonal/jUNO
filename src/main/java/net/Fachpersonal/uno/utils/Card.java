5db103689170cb7dbd12b8cecd889f3c3ead8d06
Fachpersonal <falscherIdiot@gmail.com>
2022-11-21 20:10:39 +0100

    updated Card
    bigger Deck
    special Cards now have index value of -1

--- src/main/java/net/Fachpersonal/uno/utils/Card.java
+++ src/main/java/net/Fachpersonal/uno/utils/Card.java
@@ -3,74 +3,142 @@ package net.Fachpersonal.uno.utils;
 import net.Fachpersonal.uno.exceptions.UNOERR;
 import net.Fachpersonal.uno.exceptions.UNOException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Card {
 
-    public static final HashMap<Card, Boolean> gameCards = new HashMap<>();
-    static {
+    public static final Card[] gameCards;
+    public static final ArrayList<Integer> usedCards;
 
+    static {
         try {
-            gameCards.put(new Card(Color.SPECIAL, Type.NONE), false);
-            gameCards.put(new Card(Color.RED, 0), false);
-            gameCards.put(new Card(Color.RED, 1), false);
-            gameCards.put(new Card(Color.RED, 2), false);
-            gameCards.put(new Card(Color.RED, 3), false);
-            gameCards.put(new Card(Color.RED, 4), false);
-            gameCards.put(new Card(Color.RED, 5), false);
-            gameCards.put(new Card(Color.RED, 6), false);
-            gameCards.put(new Card(Color.RED, 7), false);
-            gameCards.put(new Card(Color.RED, 8), false);
-            gameCards.put(new Card(Color.RED, 9), false);
-            gameCards.put(new Card(Color.YELLOW, 0), false);
-            gameCards.put(new Card(Color.YELLOW, 1), false);
-            gameCards.put(new Card(Color.YELLOW, 2), false);
-            gameCards.put(new Card(Color.YELLOW, 3), false);
-            gameCards.put(new Card(Color.YELLOW, 4), false);
-            gameCards.put(new Card(Color.YELLOW, 5), false);
-            gameCards.put(new Card(Color.YELLOW, 6), false);
-            gameCards.put(new Card(Color.YELLOW, 7), false);
-            gameCards.put(new Card(Color.YELLOW, 8), false);
-            gameCards.put(new Card(Color.YELLOW, 9), false);
-            gameCards.put(new Card(Color.GREEN, 0), false);
-            gameCards.put(new Card(Color.GREEN, 1), false);
-            gameCards.put(new Card(Color.GREEN, 2), false);
-            gameCards.put(new Card(Color.GREEN, 3), false);
-            gameCards.put(new Card(Color.GREEN, 4), false);
-            gameCards.put(new Card(Color.GREEN, 5), false);
-            gameCards.put(new Card(Color.GREEN, 6), false);
-            gameCards.put(new Card(Color.GREEN, 7), false);
-            gameCards.put(new Card(Color.GREEN, 8), false);
-            gameCards.put(new Card(Color.GREEN, 9), false);
-            gameCards.put(new Card(Color.BLUE, 0), false);
-            gameCards.put(new Card(Color.BLUE, 1), false);
-            gameCards.put(new Card(Color.BLUE, 2), false);
-            gameCards.put(new Card(Color.BLUE, 3), false);
-            gameCards.put(new Card(Color.BLUE, 4), false);
-            gameCards.put(new Card(Color.BLUE, 5), false);
-            gameCards.put(new Card(Color.BLUE, 6), false);
-            gameCards.put(new Card(Color.BLUE, 7), false);
-            gameCards.put(new Card(Color.BLUE, 8), false);
-            gameCards.put(new Card(Color.BLUE, 9), false);
-            gameCards.put(new Card(Color.RED, Type.SKIP), false);
-            gameCards.put(new Card(Color.YELLOW, Type.SKIP), false);
-            gameCards.put(new Card(Color.GREEN, Type.SKIP), false);
-            gameCards.put(new Card(Color.BLUE, Type.SKIP), false);
-            gameCards.put(new Card(Color.RED, Type.REVERSE), false);
-            gameCards.put(new Card(Color.YELLOW, Type.REVERSE), false);
-            gameCards.put(new Card(Color.GREEN, Type.REVERSE), false);
-            gameCards.put(new Card(Color.BLUE, Type.REVERSE), false);
-            gameCards.put(new Card(Color.RED, Type.PLUS2), false);
-            gameCards.put(new Card(Color.YELLOW, Type.PLUS2), false);
-            gameCards.put(new Card(Color.GREEN, Type.PLUS2), false);
-            gameCards.put(new Card(Color.BLUE, Type.PLUS2), false);
-            gameCards.put(new Card(Color.RED, Type.PLUS2), false);
-            gameCards.put(new Card(Color.YELLOW, Type.SWITCH), false);
-            gameCards.put(new Card(Color.GREEN, Type.SWITCH), false);
-            gameCards.put(new Card(Color.BLUE, Type.SWITCH), false);
-            gameCards.put(new Card(Color.RED, Type.SWITCH), false);
-            gameCards.put(new Card(Color.SPECIAL, Type.PLUS4), false);
-            gameCards.put(new Card(Color.SPECIAL, Type.CHANGE_COLOR), false);
+            usedCards = new ArrayList<>();
+            gameCards = new Card[]{new Card(Color.SPECIAL, Type.NONE),
+                new Card(Color.RED, 0),
+                new Card(Color.RED, 0),
+                new Card(Color.RED, 1),
+                new Card(Color.RED, 1),
+                new Card(Color.RED, 2),
+                new Card(Color.RED, 2),
+                new Card(Color.RED, 3),
+                new Card(Color.RED, 3),
+                new Card(Color.RED, 4),
+                new Card(Color.RED, 4),
+                new Card(Color.RED, 5),
+                new Card(Color.RED, 5),
+                new Card(Color.RED, 6),
+                new Card(Color.RED, 6),
+                new Card(Color.RED, 7),
+                new Card(Color.RED, 7),
+                new Card(Color.RED, 8),
+                new Card(Color.RED, 8),
+                new Card(Color.RED, 9),
+                new Card(Color.RED, 9),
+                new Card(Color.YELLOW, 0),
+                new Card(Color.YELLOW, 0),
+                new Card(Color.YELLOW, 1),
+                new Card(Color.YELLOW, 1),
+                new Card(Color.YELLOW, 2),
+                new Card(Color.YELLOW, 2),
+                new Card(Color.YELLOW, 3),
+                new Card(Color.YELLOW, 3),
+                new Card(Color.YELLOW, 4),
+                new Card(Color.YELLOW, 4),
+                new Card(Color.YELLOW, 5),
+                new Card(Color.YELLOW, 5),
+                new Card(Color.YELLOW, 6),
+                new Card(Color.YELLOW, 6),
+                new Card(Color.YELLOW, 7),
+                new Card(Color.YELLOW, 7),
+                new Card(Color.YELLOW, 8),
+                new Card(Color.YELLOW, 8),
+                new Card(Color.YELLOW, 9),
+                new Card(Color.YELLOW, 9),
+                new Card(Color.GREEN, 0),
+                new Card(Color.GREEN, 0),
+                new Card(Color.GREEN, 1),
+                new Card(Color.GREEN, 1),
+                new Card(Color.GREEN, 2),
+                new Card(Color.GREEN, 2),
+                new Card(Color.GREEN, 3),
+                new Card(Color.GREEN, 3),
+                new Card(Color.GREEN, 4),
+                new Card(Color.GREEN, 4),
+                new Card(Color.GREEN, 5),
+                new Card(Color.GREEN, 5),
+                new Card(Color.GREEN, 6),
+                new Card(Color.GREEN, 6),
+                new Card(Color.GREEN, 7),
+                new Card(Color.GREEN, 7),
+                new Card(Color.GREEN, 8),
+                new Card(Color.GREEN, 8),
+                new Card(Color.GREEN, 9),
+                new Card(Color.GREEN, 9),
+                new Card(Color.BLUE, 0),
+                new Card(Color.BLUE, 0),
+                new Card(Color.BLUE, 1),
+                new Card(Color.BLUE, 1),
+                new Card(Color.BLUE, 2),
+                new Card(Color.BLUE, 2),
+                new Card(Color.BLUE, 3),
+                new Card(Color.BLUE, 3),
+                new Card(Color.BLUE, 4),
+                new Card(Color.BLUE, 4),
+                new Card(Color.BLUE, 5),
+                new Card(Color.BLUE, 5),
+                new Card(Color.BLUE, 6),
+                new Card(Color.BLUE, 6),
+                new Card(Color.BLUE, 7),
+                new Card(Color.BLUE, 7),
+                new Card(Color.BLUE, 8),
+                new Card(Color.BLUE, 8),
+                new Card(Color.BLUE, 9),
+                new Card(Color.BLUE, 9),
+                new Card(Color.RED, Type.SKIP),
+                new Card(Color.RED, Type.SKIP),
+                new Card(Color.YELLOW, Type.SKIP),
+                new Card(Color.YELLOW, Type.SKIP),
+                new Card(Color.GREEN, Type.SKIP),
+                new Card(Color.GREEN, Type.SKIP),
+                new Card(Color.BLUE, Type.SKIP),
+                new Card(Color.BLUE, Type.SKIP),
+                new Card(Color.RED, Type.REVERSE),
+                new Card(Color.RED, Type.REVERSE),
+                new Card(Color.YELLOW, Type.REVERSE),
+                new Card(Color.YELLOW, Type.REVERSE),
+                new Card(Color.GREEN, Type.REVERSE),
+                new Card(Color.GREEN, Type.REVERSE),
+                new Card(Color.BLUE, Type.REVERSE),
+                new Card(Color.BLUE, Type.REVERSE),
+                new Card(Color.RED, Type.PLUS2),
+                new Card(Color.RED, Type.PLUS2),
+                new Card(Color.YELLOW, Type.PLUS2),
+                new Card(Color.YELLOW, Type.PLUS2),
+                new Card(Color.GREEN, Type.PLUS2),
+                new Card(Color.GREEN, Type.PLUS2),
+                new Card(Color.BLUE, Type.PLUS2),
+                new Card(Color.BLUE, Type.PLUS2),
+                new Card(Color.RED, Type.PLUS2),
+                new Card(Color.RED, Type.PLUS2),
+                new Card(Color.YELLOW, Type.SWITCH),
+                new Card(Color.YELLOW, Type.SWITCH),
+                new Card(Color.GREEN, Type.SWITCH),
+                new Card(Color.GREEN, Type.SWITCH),
+                new Card(Color.BLUE, Type.SWITCH),
+                new Card(Color.BLUE, Type.SWITCH),
+                new Card(Color.RED, Type.SWITCH),
+                new Card(Color.RED, Type.SWITCH),
+                new Card(Color.SPECIAL, Type.PLUS4),
+                new Card(Color.SPECIAL, Type.PLUS4),
+                new Card(Color.SPECIAL, Type.PLUS4),
+                new Card(Color.SPECIAL, Type.PLUS4),
+                new Card(Color.SPECIAL, Type.CHANGE_COLOR),
+                new Card(Color.SPECIAL, Type.CHANGE_COLOR),
+                new Card(Color.SPECIAL, Type.CHANGE_COLOR),
+                new Card(Color.SPECIAL, Type.CHANGE_COLOR)
+            };
+
         } catch (UNOException e) {
             throw new RuntimeException(e);
         }
@@ -92,10 +160,12 @@ public class Card {
             case REVERSE, PLUS2, SKIP, SWITCH -> {
                 this.color = color;
                 this.type = type;
+                this.index = -1;
             }
             case CHANGE_COLOR, PLUS4 -> {
                 this.color = Color.SPECIAL;
                 this.type = type;
+                this.index = -1;
             }
             case NORMAL -> throw new UNOException(UNOERR.CARD_MISSING_INDEX);
         }
