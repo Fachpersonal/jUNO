c91f027bac636b2e0899ecd00066157858116700
Fachpersonal <falscherIdiot@gmail.com>
2022-11-23 23:20:57 +0100

    reworked Client

--- src/main/java/net/Fachpersonal/uno/client/Client.java
+++ src/main/java/net/Fachpersonal/uno/client/Client.java
@@ -2,32 +2,23 @@ package net.Fachpersonal.uno.client;
 
 import net.Fachpersonal.uno.exceptions.UNOERR;
 import net.Fachpersonal.uno.exceptions.UNOException;
-import net.Fachpersonal.uno.utils.Card;
-import net.Fachpersonal.uno.utils.Player;
 
-import javax.swing.*;
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
 
+    public static boolean debug = false;
+    public static Client client = null;
     private final int MAX_USERNAME_LENGTH = 8;
     private final int MIN_USERNAME_LENGTH = 4;
-
     private final BufferedReader input;
-    private final ObjectInputStream objInput;
     private final PrintWriter output;
-    private final ObjectOutputStream objOutput;
-
-    public static boolean debug = false;
-
-    public static Client client = null;
     public Game game = null;
 
     public Client(String ip, int port) throws IOException, UNOException {
-        if(client == null) {
+        if (client == null) {
             client = this;
         } else {
             throw new UNOException(UNOERR.CANNOT_CREATE_CLIENT);
@@ -48,18 +39,16 @@ public class Client {
         }
         Socket s = new Socket(ip, port);
 
-        console("New player ["+username+"]");
+        console("New player [" + username + "]");
 
         input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        objInput = new ObjectInputStream(s.getInputStream());
         output = new PrintWriter(s.getOutputStream());
-        objOutput = new ObjectOutputStream(s.getOutputStream());
 
-        if(readLine().substring(2).equals("startGame")) {
-            if(game == null) {
+        if (readLine().substring(2).equals("startGame")) {
+            if (game == null) {
                 game = new Game();
             } else {
-
+                throw new UNOException(UNOERR.GAME_STARTED);
             }
         }
         s.close();
@@ -73,25 +62,19 @@ public class Client {
         new Client("localhost", 12345);
     }
 
-    public void write(String msg) { // writes to server
-        output.println(msg);
-        output.flush();
-    }
-    public void writeOBJ(Object obj) throws IOException {
-        objOutput.writeObject(obj);
-        objOutput.flush();
-    }
-
-    public String readLine() throws IOException { // Reads messages from server
-        return input.readLine();
-    }
-    public Object readObject() throws IOException, ClassNotFoundException {
-        return objInput.readObject();
-    }
-
     public static void console(String message) {
         if (debug) {
             System.out.println(message);
         }
     }
+
+    public void write(String msg) { // writes to server
+        output.println(msg);
+        output.flush();
+    }
+
+    public String readLine() throws IOException { // Reads messages from server
+        return input.readLine();
+    }
+
 }
